314cf18d8ce468aa5bd9643bb6ef16c31d5eda74
Jaymark Morada <jaymarkmorada20@gmail.com>
2024-10-08 08:08:40 +0000

    tasksheet113

--- Tasksheet113.java
+++ Tasksheet113.java
@@ -1,6 +1,5 @@
 public class Tasksheet113{
     public static void main(String[] args) {
-        // Declare and initialize the primitives
         byte zero = 0;
         char H = 'H';
         int three = 3;
@@ -8,10 +7,7 @@ public class Tasksheet113{
         float twoPointZero = 2.0f;
         boolean isTrue = true;
 
-        // Concatenate primitives into a string
         String output = H + "" + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + isTrue;
-
-        // Print the result
         System.out.println(output);
     }
 }
